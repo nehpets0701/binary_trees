dc6cb983d24b8c503b6b2c39576882b7902c9acc
nehpets <nehpets0701@gmail.com>
2021-03-09 20:46:52 +0000

    betty

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -20,5 +20,5 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	grandparent = node->parent->parent;
 	if (parent == grandparent->left)
 		return (grandparent->right);
-	return (grandparent->left);	
+	return (grandparent->left);
 }
