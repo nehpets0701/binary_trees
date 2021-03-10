fc9d2875bd9c840fcb682f10551b1486415aaf64
epaschke22 <ericksonpaschke@gmail.com>
2021-03-10 19:44:48 +0000

    updating task 19

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,24 +1,15 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_depth-counts tree depth
- * Return:size_t depth
- * @tree:input
+ * binary_tree_depth - counts tree depth
+ * Return: depth from node
+ * @tree: input
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	const binary_tree_t *current = tree;
-	int depth = 0;
-
-	if (tree == NULL)
+	if (tree == NULL || tree->parent == NULL)
 		return (0);
-
-	while (current->parent != NULL)
-	{
-		current = current->parent;
-		depth++;
-	}
-	return (depth);
+	return (1 + binary_tree_depth(tree->parent));
 }
 
 /**
