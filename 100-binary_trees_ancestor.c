9bdfe8c952fbc59b2112cbd05f53ecfd5e25fb63
epaschke22 <ericksonpaschke@gmail.com>
2021-03-10 19:46:42 +0000

    updating task 19

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -23,7 +23,7 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 {
 	size_t fdepth, sdepth;
 
-	if (first == NULL && second == NULL)
+	if (first == NULL || second == NULL)
 		return (NULL);
 	if (first == second)
 		return ((binary_tree_t *)first);
